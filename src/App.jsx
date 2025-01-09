4b8ab09b6f205e4a83bc610512d383a02eded096
Ridyl <joey.dalatri214@gmail.com>
2025-01-09 15:04:27 -0500

    Added logic to create new todo items (job, description, priority), which are then rendered in a temporary <ul>. All todo items have default value for editing set to false.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,101 @@
+import { useState } from 'react'
 import './App.css'
 
+// Item framework - default low pri in the case of no change
+const initialItem = {
+  job: '',
+  desc: '',
+  pri: '',
+  edit: false,
+};
+
 function App() {
+  const [todoList, addTodoList] = useState([]);
+  const [listItem, setListItem] = useState(initialItem);
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setListItem({
+      ...listItem,
+      [name]: value,
+    });
+  }
+
+  function handleAdd() {
+    // Push to array for later access and rendering
+    addTodoList(prevList => [...prevList, listItem]);
+    // Reset listItem to initialItem to accept more
+    setListItem(initialItem);
+  }
 
   // MOVE ALL THIS TO SEPARATE COMPONENT FILES
   return (
     <>
-    {/* Header grouping */}
+      {/* Header grouping */}
       <div className="header-group">
-        <h1 className='header'>Very Simple Todo App</h1>
-        <h4 className='sub-header'>Track All of the Things!</h4>
+        <h1 className="header">Very Simple Todo App</h1>
+        <h4 className="sub-header">Track All of the Things!</h4>
       </div>
 
       {/* Left side basic form */}
       {/* Will create a Bootstrap Card */}
       <div>
-        <form className='add-todo'>
-          <label htmlFor='job'>Title,</label>
-          <input id='job' type='text' placeholder='Job to do...'></input>
-          <label htmlFor='desc'>Description,</label>
-          <input id='desc' type='text' placeholder='How to do it...'></input>
-          <label htmlFor='pri'>Priority,</label>
+        <form className="add-todo">
+          <label htmlFor="job">Title,</label>
+          <input
+            name="job"
+            value={listItem.job}
+            onChange={handleChange}
+            placeholder="Job to do..."
+          />
+
+          <label htmlFor="desc">Description,</label>
+          <input
+            name="desc"
+            value={listItem.desc}
+            onChange={handleChange}
+            placeholder="How to do it..."
+          />
+
+          <label htmlFor="pri">Priority,</label>
           {/* Turn <select> into Bootstrap DropDown */}
-          <select id='pri'>
-            <option value={'low'}>Low</option>
-            <option value={'med'}>Medium</option>
-            <option value={'high'}>High</option>
+          <select
+            value={listItem.pri}
+            name="pri"
+            onChange={(e) => handleChange(e)}
+          >
+            <option value={1}>Low</option>
+
+            <option value={2}>Med</option>
+
+            <option value={3}>High</option>
+            
           </select>
-          <button type='button'>Add to List</button>
+          <button type="button" onClick={handleAdd}>
+            Add to List
+          </button>
         </form>
       </div>
 
       {/* Vertical Bootstrap card groups with navigation to edit
       and delete button on right side of header */}
-      <div className='task-area'>
+      <div className="task-area">
         {/* will create card component that dynamically updates based off of priority selected 
         ul placeholder for now to get code working*/}
+        <ul className="test-list">
+          {todoList.map((item, i) => {
+            return (
+              <li key={i}>
+                <h5>{item.job}</h5>
+                <p>{item.desc}</p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
-  )
+  );
 }
 
 export default App
